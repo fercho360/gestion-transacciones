8065a6296d25c96eb85a8ff14501dc6cddf04c4b
Fernando-dev <f.vadi27@gmail.com>
2026-04-28 18:38:29 -0600

    Correcciones

--- src/main/java/com/eglobal/gestion_transacciones/repository/BancoRepository.java
+++ src/main/java/com/eglobal/gestion_transacciones/repository/BancoRepository.java
@@ -4,13 +4,6 @@ import com.eglobal.gestion_transacciones.entity.BancoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface BancoRepository extends JpaRepository<BancoEntity, Integer> {
-    List<BancoEntity> findAll();
-    Optional<BancoEntity> findById(Integer id);
-    BancoEntity save(BancoEntity banco);
-    void deleteById(Integer id);
 }
